5aeb6b9be8ef32715755d02def9d99002eae52e4
TariqT777 <TariqThomas777@gmail.com>
2021-11-03 17:42:43 -0500

    Added a templates directory to begin creating our Django views.

--- Chatbot/Chatbot/urls.py
+++ Chatbot/Chatbot/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include,path
 
-urlpatterns = [
+urlpatterns = [ 
     path('chatbotDialogue/', include('chatbotDialogue.urls')),
     path('admin/', admin.site.urls),
 ]
