65c6d2918dbaab131a0ca4e4786fbebf6bb3fc28
TariqT777 <TariqThomas777@gmail.com>
2021-11-04 22:42:05 -0500

    Added comments for clarity.

--- Chatbot/chatbotDialogue/views.py
+++ Chatbot/chatbotDialogue/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
 
+#This is what is currently displayed to the dialogue screen.
 def index(request):
     return HttpResponse("Hello, world. You're at the dialogue index.")
