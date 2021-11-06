94423a4fc15f5a2ed4d9ad14f3b6fcf07112929a
TariqT777 <TariqThomas777@gmail.com>
2021-11-06 16:51:03 -0500

    Fixed bug that was causing a 404 error for the index.

--- Chatbot/Chatbot/urls.py
+++ Chatbot/Chatbot/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
-
+from chatbotDialogue import views
 urlpatterns = [ 
+    path('', views.index, name='index'),
     path('chatbotDialogue/', include('chatbotDialogue.urls')),
     path('admin/', admin.site.urls),
 ]
